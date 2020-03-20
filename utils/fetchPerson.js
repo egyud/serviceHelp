8763c46366ce47b09b35b3a2b86016a3b36dc2f0
Andy <andy.egyud@gmail.com>
2020-03-20 09:15:30 -0700

    Changed url for get request

--- utils/fetchPerson.js
+++ utils/fetchPerson.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.fetchPerson = async () => {
-  let response = await axios.get('http://localhost:3000/api/person')
+  let response = await axios.get('http://helptemecula.com/api/person')
   console.log(response.data.person)
   return response.data.person[0]
 }
